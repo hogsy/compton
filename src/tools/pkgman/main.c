decdb4ed4bff4262be64f95d42441d3272d52335
hogsy <markelswo@gmail.com>
2020-06-27 20:53:44 +0100

    add pkgman utility

---
+++ src/tools/pkgman/main.c
@@ -0,0 +1,249 @@
+/* Copyright (C) 2020 Mark Sowden <markelswo@gmail.com>
+ * Project Yin
+ * */
+
+#include <PL/platform.h>
+#include <PL/platform_filesystem.h>
+#include <PL/platform_image.h>
+
+#include "main.h"
+
+/* PkgMan, the shitty package generator! */
+
+#define PKG_IDENTIFIER	"PKG1"
+
+typedef struct PkgHeader {
+	char 		identifier[ 4 ];
+	uint32_t	numFiles;
+} PkgHeader;
+static PkgHeader packageHeader = {
+		.identifier = PKG_IDENTIFIER,
+		.numFiles	= 0
+};
+
+static FILE *fileOutPtr = NULL;
+static char outputPath[ 32 ] = { '\0' };
+
+static const char *SkipSpaces( const char *buffer ) {
+	while( *buffer == ' ' ) {
+		buffer++;
+	}
+
+	return buffer;
+}
+
+static const char *SkipLine( const char *buffer ) {
+	while( *buffer != '\0' && *buffer != '\n' ) {
+		buffer++;
+	}
+
+	return ( *buffer == '\n' ) ? ++buffer : buffer;
+}
+
+static const char *ReadString( const char *buffer, char *destination, size_t length ) {
+	bool isContained = false;
+	if ( *buffer == '"' ) {
+		isContained = true;
+		buffer++;
+	}
+
+	unsigned int destPos = 0;
+	while ( *buffer != '\0' ) {
+		if ( ( *buffer == '\r' || *buffer == '\n' ) || ( isContained && *buffer == '"' ) || ( !isContained && *buffer == ' ' ) ) {
+			buffer++;
+			break;
+		}
+
+		destination[ destPos++ ] = *buffer;
+		if ( destPos >= length ) {
+			Error( "Attempted to write string larger than destination size!\n" );
+		}
+
+		buffer++;
+	}
+
+	destination[ destPos ] = '\0';
+	return SkipSpaces( buffer );
+}
+
+static void Pkg_AddFile( const char *filePath, const char *fileTag, const char *fileName ) {
+	Print( "Adding %s (%s)...\n", fileName, fileTag );
+
+	/* so we can do some path/name mangling */
+	char packPath[ PL_SYSTEM_MAX_PATH ];
+	strncpy( packPath, filePath, sizeof( packPath ) );
+	char packName[ 64 ];
+	strncpy( packName, fileName, sizeof( packName ) );
+
+	/* see if it's a file we can pack */
+	const char *extension = plGetFileExtension( packPath );
+	if ( strcmp( extension, "png" ) == 0 || strcmp( extension, "gif" ) == 0 || strcmp( extension, "bmp" ) == 0 ) {
+		PLImage image;
+		if ( !plLoadImage( packPath, &image ) ) {
+			Error( "Failed to load \"%s\"!\nPL: %s\n", packPath, plGetError() );
+		}
+
+		size_t strPos;
+		strPos = strlen( packPath ) - 4;
+		strncpy( packPath + strPos, ".gfx", 4 );
+
+		Print( "Converting \"%s\" to \"%s\"\n", filePath, packPath );
+
+		PackImage_Write( packPath, &image );
+
+		plFreeImage( &image );
+	}
+
+	PLFile *filePtr = plOpenFile( packPath, true );
+	if ( filePtr == NULL ) {
+		Error( "Failed to add file \"%s\"!\nPL: %s\n", packPath, plGetError() );
+	}
+
+	const uint8_t *data = plGetFileData( filePtr );
+	size_t fileLength = plGetFileSize( filePtr );
+
+	char indexName[ 128 ];
+	snprintf( indexName, sizeof( indexName ), "%s:%s", fileTag, packName );
+
+	/* write the index header */
+	uint8_t nameLength = ( uint8_t ) strlen( indexName );
+	fwrite( &nameLength, sizeof( uint8_t ), 1, fileOutPtr );
+	char *name = malloc( nameLength );
+	strncpy( name, indexName, nameLength );
+	fwrite( name, sizeof( char ), nameLength, fileOutPtr );
+	free( name );
+	fwrite( &fileLength, sizeof( uint32_t ), 1, fileOutPtr );
+
+	/* and now write out the file itself */
+	fwrite( data, 1, fileLength, fileOutPtr );
+
+	plCloseFile( filePtr );
+
+	packageHeader.numFiles++;
+}
+
+/**
+ * Callback used by ScanDirectory function.
+ */
+static void Pkg_AddFileCallback( const char *filePath, void *userData ) {
+	const char *fileName = plGetFileName( filePath );
+	if ( fileName == NULL ) {
+		Error( "Failed to get valid file name from \"%s\"!\n", filePath );
+	}
+
+#if 0 /* nevermind, let's include extensions */
+	const char *fileExtension = plGetFileExtension( fileName );
+	if ( fileExtension == NULL ) {
+		Error( "Failed to get valid file extension from \"%s\"!\n", filePath );
+	}
+
+	size_t length = strlen( fileName ) - ( strlen( fileExtension ) + 1 );
+	if ( length >= 16 ) {
+		Error( "File name \"%s\" is too long!\n", fileName );
+	}
+#else
+	unsigned int length = strlen( fileName );
+#endif
+
+	/* now produce a copy of the filename without the extension... */
+	char packFileName[ 64 ];
+	strncpy( packFileName, fileName, length );
+	packFileName[ length ] = '\0';
+
+	const char *tag = userData;
+	Pkg_AddFile( filePath, tag, packFileName );
+}
+
+static void ParseScript( const char *buffer, size_t length ) {
+	const char *curPos = buffer;
+	while( curPos != NULL && *curPos != '\0' ) {
+		if ( *curPos == ';' ) { /* comment */
+			curPos = SkipLine( curPos );
+			continue;
+		} else if ( strncmp( curPos, "output ", 7 ) == 0 ) { /* set output dir */
+			curPos += 7;
+			curPos = SkipSpaces( curPos );
+
+			if ( outputPath[ 0 ] != '\0' ) {
+				Error( "Output was already specified previously in script!\n" );
+			}
+
+			/* fetch the output path we want */
+			curPos = ReadString( curPos, outputPath, sizeof( outputPath ) );
+
+			Print( "OUTPUT: %s\n", outputPath );
+
+			fileOutPtr = fopen( outputPath, "wb" );
+			if ( fileOutPtr == NULL ) {
+				Error( "Failed to open \"%s\" for writing!\n", outputPath );
+			}
+
+			/* write out the file header */
+			fwrite( &packageHeader, sizeof( PkgHeader ), 1, fileOutPtr );
+
+			continue;
+		} else if ( strncmp( curPos, "add ", 4 ) == 0 ) { /* add file */
+			curPos += 4;
+			curPos = SkipSpaces(curPos);
+
+			char filePath[PL_SYSTEM_MAX_PATH];
+			curPos = ReadString(curPos, filePath, sizeof(filePath));
+
+			char fileTag[64];
+			curPos = ReadString(curPos, fileTag, sizeof(fileTag));
+
+			char fileName[64];
+			curPos = ReadString(curPos, fileName, sizeof(fileName));
+
+			Pkg_AddFile(filePath, fileTag, fileName);
+			continue;
+		} else if ( strncmp( curPos, "dir ", 4 ) == 0 ) {
+			curPos += 4;
+			curPos = SkipSpaces( curPos );
+
+			char directory[ PL_SYSTEM_MAX_PATH ];
+			curPos = ReadString( curPos, directory, sizeof( directory ) );
+
+			char extension[ 8 ];
+			curPos = ReadString( curPos, extension, sizeof( extension ) );
+
+			char tag[ 64 ];
+			curPos = ReadString( curPos, tag, sizeof( tag ) );
+
+			plScanDirectory( directory, extension, Pkg_AddFileCallback, false, tag );
+			continue;
+		}
+
+		curPos++;
+	}
+
+	fseek( fileOutPtr, SEEK_SET, 0 );
+	fwrite( &packageHeader, sizeof( PkgHeader ), 1, fileOutPtr );
+	fclose( fileOutPtr );
+}
+
+int main( int argc, char **argv ) {
+	plInitialize( argc, argv );
+
+	Print( "Package Manager\nCopyright (C) 2020 Mark Sowden\n" );
+	if ( argc < 2 ) {
+		Print( "Please provide a package script!\nExample: pkgman myscript.txt\n" );
+		return EXIT_SUCCESS;
+	}
+
+	/* open the file and read it all into memory */
+	const char *input = argv[ 1 ];
+	PLFile *filePtr = plOpenFile( input, true );
+	if ( filePtr == NULL ) {
+		Error( "Failed to open \"%s\"!\nPL: %s\n", argv[ 1 ], plGetError() );
+	}
+
+	/* now fetch the buffer and length, and throw it to our parser */
+	const char *buffer = ( const char* ) plGetFileData( filePtr );
+	size_t length = plGetFileSize( filePtr );
+	ParseScript( buffer, length );
+
+	plCloseFile( filePtr );
+
+	Print( "Done!\n" );
+}
---
+++ src/tools/pkgman/pack_image.c
@@ -0,0 +1,210 @@
+/* Copyright (C) 2020 Mark Sowden <markelswo@gmail.com>
+ * Project Yin
+ * */
+
+#include <PL/platform_image.h>
+
+#include "main.h"
+
+/* Future Improvements
+ * - if the number of colours in the block are less than 16 bytes, we need to pack the data smaller
+ * - if there are two colours that aren't discernably different, pack them together (optional)
+ */
+
+#define CHANNEL_RED		( 1 << 0 )
+#define CHANNEL_GREEN	( 1 << 1 )
+#define CHANNEL_BLUE	( 1 << 2 )
+#define CHANNEL_ALPHA	( 1 << 3 )
+
+#define GFX_IDENTIFIER	"GFX0"
+
+static uint8_t PackImage_GetNumChannels( uint8_t channelFlags ) {
+	uint8_t numChannels = 0;
+	if ( channelFlags & CHANNEL_RED ) 	numChannels++;
+	if ( channelFlags & CHANNEL_GREEN ) numChannels++;
+	if ( channelFlags & CHANNEL_BLUE ) 	numChannels++;
+	if ( channelFlags & CHANNEL_ALPHA )	numChannels++;
+	return numChannels;
+}
+
+static void PackImage_WriteHeader( FILE *filePtr, uint8_t channels, uint16_t width, uint16_t height, uint16_t numBlocks ) {
+	/* make sure we're at the start */
+	fseek( filePtr, 0, SEEK_SET );
+
+	char identifier[ 4 ] = GFX_IDENTIFIER;
+	fwrite( identifier, sizeof( char ), 4, filePtr );
+	fwrite( &channels, sizeof( uint8_t ), 1, filePtr );
+	fwrite( &width, sizeof( uint16_t ), 1, filePtr );
+	fwrite( &height, sizeof( uint16_t ), 1, filePtr );
+	/* if this returns 0, it means there's just plain data */
+	fwrite( &numBlocks, sizeof( uint16_t ), 1, filePtr );
+}
+
+static void PackImage_WriteBlock( FILE *filePtr, const uint8_t *colour, uint8_t channelFlags, uint8_t numChannels, const uint8_t *srcBuffer, uint16_t srcPixelSize ) {
+	/* figure out how many channels we need for this block */
+	uint8_t blockChannelFlags = 0;
+	for ( unsigned int i = 0; i < numChannels; ++i ) {
+		/* hackity hack, figure out how many channels we're actually using */
+		if ( colour[ i ] > 0 && i < 3 ) {
+			uint8_t specificChannel = 0;
+			if ( i == 0 ) 		{ specificChannel = CHANNEL_RED; }
+			else if ( i == 1 ) 	{ specificChannel = CHANNEL_GREEN; }
+			else if ( i == 2 ) 	{ specificChannel = CHANNEL_BLUE; }
+			blockChannelFlags |= specificChannel;
+		} else if ( colour[ i ] != 255 && i == 3 ) {
+			blockChannelFlags |= CHANNEL_ALPHA;
+		}
+	}
+
+	fputc( blockChannelFlags, filePtr );
+	if ( blockChannelFlags & CHANNEL_RED ) 		{ fputc( colour[ 0 ], filePtr ); }
+	if ( blockChannelFlags & CHANNEL_GREEN ) 	{ fputc( colour[ 1 ], filePtr ); }
+	if ( blockChannelFlags & CHANNEL_BLUE )		{ fputc( colour[ 2 ], filePtr ); }
+	if ( blockChannelFlags & CHANNEL_ALPHA ) 	{ fputc( colour[ 3 ], filePtr ); }
+
+	/* now figure out how many pixels there are that we need in this block */
+	uint16_t numBlockPixels = 0;
+	uint16_t *pixelOffsets = malloc( sizeof( uint16_t ) * srcPixelSize );
+	for ( uint16_t i = 0; i < srcPixelSize; ++i ) {
+		uint8_t srcColour[ 4 ];
+		memcpy( srcColour, srcBuffer, sizeof( uint8_t ) * numChannels );
+
+		bool rgba[ 4 ] = { true, true, true, true };
+		for ( uint8_t j = 0; j < numChannels; ++j ) {
+			rgba[ j ] = ( srcColour[ j ] == colour[ j ] );
+		}
+
+		srcBuffer += numChannels;
+
+		if ( !( rgba[ 0 ] && rgba[ 1 ] && rgba[ 2 ] && rgba[ 3 ] ) ) {
+			continue;
+		}
+
+		pixelOffsets[ numBlockPixels++ ] = i;
+	}
+
+	if ( numBlockPixels == 0 ) {
+		Error( "Invalid pixel block, num pixels returned as 0!\n" );
+	}
+
+	/* and write out the pixel offsets */
+	fwrite( &numBlockPixels, sizeof( uint16_t ), 1, filePtr );
+	fwrite( pixelOffsets, sizeof( uint16_t ), numBlockPixels, filePtr );
+
+	free( pixelOffsets );
+}
+
+void PackImage_Write( const char *path, const PLImage *image ) {
+	if ( image->width >= INT16_MAX || image->height >= INT16_MAX ) {
+		Error( "Image is too large, maximum size is %dx%d!\n", INT16_MAX, INT16_MAX );
+	}
+
+	FILE *filePtr = fopen( path, "wb" );
+	if ( filePtr == NULL ) {
+		Error( "Failed to open \"%s\" for writing!\n", path );
+	}
+
+	/* figure out how many channels the image is using */
+	uint8_t channels = 0;
+	switch( image->format ) {
+		default: Error( "Unhandled pixel format for \"%s\"!\n", image->path );
+		case PL_IMAGEFORMAT_RGBA8: 	channels |= CHANNEL_ALPHA;
+		case PL_IMAGEFORMAT_RGB8:	channels |= CHANNEL_RED | CHANNEL_GREEN | CHANNEL_BLUE;
+		break;
+	}
+
+	uint8_t numChannels = PackImage_GetNumChannels( channels );
+
+	Print( "Checking number of unique pixels in \"%s\"...\n", image->path );
+
+	unsigned int imagePixelSize = image->width * image->height;
+
+	/* figure out how many unique colours there are
+	 * so we know how many blocks there should be */
+	const uint8_t *pixelPos = image->data[ 0 ];
+	uint8_t *colourBuffer = calloc( image->size, sizeof( uint8_t ) );
+	uint16_t numColours = 0;
+	uint8_t outputChannels = 0;
+	for( unsigned int i = 0; i < imagePixelSize; ++i ) {
+		/* copy the initial colour to kick us off */
+		if ( numColours == 0 ) {
+			memcpy( &colourBuffer[ numColours * numChannels ], pixelPos, numChannels );
+			numColours++;
+			continue;
+		}
+
+		/* check whether or not the colour is already in the table */
+		bool rgba[ 4 ] = { true, true, true, true };
+		for ( unsigned int j = 0; j < numColours; ++j ) {
+			for ( unsigned int k = 0; k < numChannels; ++k ) {
+				rgba[ k ] = ( colourBuffer[ j * numChannels + k ] == pixelPos[ k ] );
+
+				/* hackity hack, figure out how many channels we're actually using */
+				/* todo: refer back to a table to ensure we're setting the correct channels here */
+				if ( colourBuffer[ j * numChannels + k ] > 0 && k < 3 ) {
+					uint8_t specificChannel = 0;
+					if ( k == 0 ) 		{ specificChannel = CHANNEL_RED; }
+					else if ( k == 1 ) 	{ specificChannel = CHANNEL_GREEN; }
+					else if ( k == 2 ) 	{ specificChannel = CHANNEL_BLUE; }
+					outputChannels |= specificChannel;
+				} else if ( colourBuffer[ j * numChannels + k ] != 255 && k == 3 ) {
+					outputChannels |= CHANNEL_ALPHA;
+				}
+			}
+
+			if ( rgba[ 0 ] && rgba[ 1 ] && rgba[ 2 ] && rgba[ 3 ] ) {
+				break;
+			}
+		}
+
+		/* colours didn't match, so it's a new unique pixel! */
+		if ( !( rgba[ 0 ] && rgba[ 1 ] && rgba[ 2 ] && rgba[ 3 ] ) ) {
+			memcpy(&colourBuffer[ numColours * numChannels ], pixelPos, numChannels );
+			numColours++;
+		}
+
+		pixelPos += numChannels;
+	}
+
+	Print( "Found %d unique pixels\n", numColours );
+
+	uint16_t numBlocks = numColours;
+#if 1
+	if ( numBlocks > 2048 ) {
+		Print( "Skipping optimisation!\n" );
+		numBlocks = 0;
+	} else {
+		Print( "Proceeding to pack image...\n" );
+	}
+#else
+	Print( "Proceeding to pack image...\n" );
+	numBlocks = 0;
+#endif
+
+	Print( "ChFl. %d, BlNum. %d, W. %d, H. %d\n", outputChannels, numBlocks, image->width, image->height );
+
+	/* go ahead and write out the header */
+	PackImage_WriteHeader( filePtr, outputChannels, image->width, image->height, numBlocks );
+
+	if ( numBlocks == 0 ) {
+		/* no blocks, so just go straight to the data */
+		pixelPos = image->data[ 0 ];
+		for ( unsigned int i = 0; i < imagePixelSize; ++i ) {
+			/* write out each channel we're using */
+			if ( outputChannels & CHANNEL_RED ) 	{ fputc(pixelPos[ 0 ], filePtr); }
+			if ( outputChannels & CHANNEL_GREEN ) 	{ fputc( pixelPos[ 1 ], filePtr ); }
+			if ( outputChannels & CHANNEL_BLUE ) 	{ fputc( pixelPos[ 2 ], filePtr ); }
+			if ( outputChannels & CHANNEL_ALPHA ) 	{ fputc( pixelPos[ 3 ], filePtr ); }
+			pixelPos += numChannels;
+		}
+	} else {
+		for (unsigned int i = 0; i < numColours; ++i) {
+			PackImage_WriteBlock( filePtr, &colourBuffer[ i * numChannels ], outputChannels, numChannels, image->data[ 0 ], imagePixelSize );
+		}
+	}
+
+	free(colourBuffer );
+	fclose( filePtr );
+
+	Print( "Wrote \"%s\"\n", path );
+}

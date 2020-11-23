0dd38ec973e136e5cecb132723dfac09561403b0
hogsy <markelswo@gmail.com>
2020-11-23 21:32:00 +0000

    Temporarily

--- bin/scripts/test.asm
+++
@@ -1,7 +0,0 @@
-; defs <id> "<string>"
-defs 0 "Hello World"    ; set string register 0 to 'Hello World'
-; defi <id> <integer>
-defi num 
-
-loadi 0 100             ; load 100 into register 0
-addi 0 10               ; add 10 to register 0

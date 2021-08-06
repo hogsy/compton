f642f989e06ea1008664e9aa13ebba20f1d88519
Mark Sowden <markelswo@gmail.com>
2021-08-06 15:35:02 +0100

    Fix assert macro.

--- src/engine/SimGame.h
+++ src/engine/SimGame.h
@@ -12,33 +12,33 @@
 #include <vector>
 #include <map>
 
-extern int VC_LOG_MSG; // generic message
-extern int VC_LOG_DEB; // debug message (won't be displayed in shipped build)
-extern int VC_LOG_WAR; // warning
-extern int VC_LOG_ERR; // error (kills application)
+extern int VC_LOG_MSG;// generic message
+extern int VC_LOG_DEB;// debug message (won't be displayed in shipped build)
+extern int VC_LOG_WAR;// warning
+extern int VC_LOG_ERR;// error (kills application)
 
-#define Print( ... )       \
+#define Print( ... ) \
 	PlLogMessage( VC_LOG_MSG, __VA_ARGS__ )
-#define Warning( ... )     \
+#define Warning( ... ) \
 	PlLogMessage( VC_LOG_WAR, __VA_ARGS__ )
 #define Error( ... )                         \
 	PlLogMessage( VC_LOG_ERR, __VA_ARGS__ ); \
 	exit( -1 )
 #if defined( DEBUG_BUILD )
-#   define DebugMsg( ... )    \
+#define DebugMsg( ... ) \
 	PlLogMessage( VC_LOG_DEB, __VA_ARGS__ )
 #else
-#   define DebugMsg( ... )
+#define DebugMsg( ... )
 #endif
 
 #if defined( DEBUG_BUILD )
-#   define SgAssert( A, ... )   DebugMsg( __VA_ARGS__ ); assert( A )
+#define SgAssert( A, ... ) assert( ( __VA_ARGS__, A ) )
 #else
-#   define SgAssert( A, ... )
+#define SgAssert( A, ... )
 #endif
 
 #define IMPLEMENT_SUPER( CLASS ) typedef CLASS SuperClass;
 
-#define FONT_SIZE_LARGE     50
-#define FONT_SIZE_MEDIUM    32
-#define FONT_SIZE_SMALL     8
+#define FONT_SIZE_LARGE	 50
+#define FONT_SIZE_MEDIUM 32
+#define FONT_SIZE_SMALL	 8

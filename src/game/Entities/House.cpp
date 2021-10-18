dba0979542e460f50525b9b8229ee1656030dbd3
Mark Sowden <markelswo@gmail.com>
2021-10-18 15:52:05 +0100

    GPLv3 copyright for files.

--- src/game/Entities/House.cpp
+++ src/game/Entities/House.cpp
@@ -1,6 +1,20 @@
-//
-// Created by mark- on 08/11/2020.
-//
+/*
+SimGame Engine
+Copyright (C) 2016-2021 Mark E Sowden <hogsy@oldtimes-software.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "SimGame.h"
 #include "House.h"
--- src/game/vc_old/object_world.h
+++ src/game/vc_old/object_world.h
@@ -1,3 +1,20 @@
+/*
+SimGame Engine
+Copyright (C) 2016-2021 Mark E Sowden <hogsy@oldtimes-software.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 // Virtual Critters, Copyright (C) 2016 Mark Elsworth Sowden
 
 #pragma once
@@ -5,11 +22,8 @@
 #include "object_creature.h"
 
 class CloudObject;
-
 class Moon;
-
 class Sun;
-
 class World;
 
 extern World *game_worldmanager;
